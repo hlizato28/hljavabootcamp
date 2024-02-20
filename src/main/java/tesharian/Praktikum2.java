d3bc12655d79658b2e6f52c7fb858be80ff63679
hlizato28 <heinrichlizato28@gmail.com>
2024-02-20 09:26:02 +0700

    Praktikum 2

---
+++ src/main/java/tesharian/Praktikum2.java
@@ -0,0 +1,30 @@
+package tesharian;
+/*
+IntelliJ IDEA 2022.3.1 (Community Edition)
+Build #IC-223.8214.52, built on December 20, 2022
+@Author Heinrich a.k.a. Heinrich Lizato
+Java Developer
+Created on 20/02/2024 08:51
+@Last Modified 20/02/2024 08:51
+Version 1.0
+*/
+
+import java.util.Scanner;
+
+public class Praktikum2 {
+    public static void main(String[] args) {
+        double nilai = 0;
+        int jumlahAngka = 0;
+        boolean a = true;
+        Scanner sc = new Scanner(System.in);
+        try {
+            while (a) {
+                nilai += sc.nextInt();
+                jumlahAngka += 1;
+            }
+        } catch (Exception e) {
+            System.out.println(nilai / jumlahAngka);
+        }
+    }
+}
+
